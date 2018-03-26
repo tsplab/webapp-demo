10d818fdd4a8be8a5f1cef25f4ba34b1138c20a2
Edouard Gamelin <edgame@microsoft.com>
2018-03-26 10:27:38 +0100

    first commit

--- index.php
+++ index.php
@@ -161,8 +161,6 @@
   src="https://code.jquery.com/jquery-3.3.1.min.js"
   integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   crossorigin="anonymous"></script>
-
-
     <script src="../assets/js/jquery.js"></script>
     <script src="../assets/js/bootstrap-transition.js"></script>
     <script src="../assets/js/bootstrap-alert.js"></script>
